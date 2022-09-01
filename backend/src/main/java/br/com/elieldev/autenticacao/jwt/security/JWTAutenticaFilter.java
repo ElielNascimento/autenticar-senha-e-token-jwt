8b5e03244c128038088ad4c593ce2e9858b3587d
ElielNascimento <nascimentob.eliel@gmail.com>
2022-09-01 17:04:20 -0300

    updtade projeto

---
+++ backend/src/main/java/br/com/elieldev/autenticacao/jwt/security/JWTAutenticaFilter.java
@@ -0,0 +1,65 @@
+package br.com.elieldev.autenticacao.jwt.security;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import br.com.elieldev.autenticacao.jwt.data.DetalhesUsuarioData;
+import br.com.elieldev.encriptarsenhausuario.model.UsuarioModel;
+
+public class JWTAutenticaFilter extends UsernamePasswordAuthenticationFilter {
+
+	public static final int TOKEN_EXPIRACAO = 600_00;
+
+	public static final String TOKEN_SENHA = "b9fafeb7-e632-4d6f-ab2f-1853a02e4a85";
+
+	private final AuthenticationManager authenticationManager;
+
+	public JWTAutenticaFilter(AuthenticationManager authenticationManager) {
+		this.authenticationManager = authenticationManager;
+	}
+
+	@Override
+	public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) {
+
+		try {
+			UsuarioModel usuario = new ObjectMapper().readValue(request.getInputStream(), UsuarioModel.class);
+
+			return authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(usuario.getLogin(),
+					usuario.getPassword(), new ArrayList<>()));
+
+		} catch (IOException e) {
+			throw new RuntimeException("Falha ao autenticar usuario", e);
+		}
+
+	}
+
+	@Override
+	protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
+			Authentication authResult) throws IOException, ServletException {
+
+		DetalhesUsuarioData usuarioData = (DetalhesUsuarioData) authResult.getPrincipal();
+
+		String token = JWT.create().withSubject(usuarioData.getUsername())
+				.withExpiresAt(new Date(System.currentTimeMillis() + TOKEN_EXPIRACAO))
+				.sign(Algorithm.HMAC512(TOKEN_SENHA));
+
+		response.getWriter().write(token);
+		response.getWriter().flush();
+
+	}
+}
